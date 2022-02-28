11c5f65fb5f72430f02451b61bac2ce7901d179d
Gjorgjioska <marina.gorgioska@gmail.com>
2022-02-28 19:07:34 +0100

    Creating redux reducers

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,21 @@
+import { ActionTypes } from "../actions/action-types";
+
+const initiaState = {
+  products: [
+    {
+      id: 1,
+      title: "Dipesh",
+      category: "programmer",
+    },
+  ],
+};
+
+export const productReducer = (state, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state;
+
+    default:
+      return state;
+  }
+};
